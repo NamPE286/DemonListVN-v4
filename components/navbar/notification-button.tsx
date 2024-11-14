511e4b1f30b5c2f2d398894d1b5e99b1a98455f4
NamPE286 <nambuihung654@gmail.com>
2024-11-15 01:48:17 +0700

    refactor: enhance NotificationButton with Popover for notifications; update ProfileButton to manage sign-in state and add new record submission option

--- components/navbar/notification-button.tsx
+++ components/navbar/notification-button.tsx
@@ -1,10 +1,49 @@
 import { IconBell } from "@tabler/icons-react";
-import { Button } from "@nextui-org/react";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Button,
+  Card,
+  CardBody,
+} from "@nextui-org/react";
 
 export const NotificationButton = () => {
   return (
-    <Button isIconOnly className="bg-transparent rounded-full">
-        <IconBell color="gray"/>
-    </Button>
+    <Popover placement="bottom">
+      <PopoverTrigger>
+        <Button isIconOnly className="rounded-full bg-transparent">
+          <IconBell color="gray" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[350px] max-w-full px-[15px] pb-[15px]">
+        <div className="flex w-full items-center px-1 py-2">
+          <div className="text-medium font-bold">Notification</div>
+          <Button className="ml-auto bg-transparent text-foreground-500">
+            Clear
+          </Button>
+        </div>
+        <div className="flex flex-col gap-[10px]">
+          <Card>
+            <CardBody>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                felis elit, rutrum a bibendum id, lacinia vitae metus.
+              </p>
+              <p className="text-gray-500">1/1/2025</p>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardBody>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                felis elit, rutrum a bibendum id, lacinia vitae metus.
+              </p>
+              <p className="text-gray-500">1/1/2025</p>
+            </CardBody>
+          </Card>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 };
--- components/navbar/profile-button.tsx
+++ components/navbar/profile-button.tsx
@@ -9,27 +9,34 @@ import {
   Divider,
 } from "@nextui-org/react";
 
-import { IconUser, IconSend, IconLogout } from "@tabler/icons-react";
-import { ReactElement } from "react";
+import {
+  IconUser,
+  IconSend,
+  IconLogout,
+  IconPlus,
+  IconBrandGoogleFilled,
+} from "@tabler/icons-react";
+import { ReactElement, useState } from "react";
 
 interface NavLink {
-  onClick: () => void;
+  onPress: () => void;
   icon: ReactElement;
   title: string;
   textColor: string;
 }
 
 export const ProfileButton = () => {
+  const [isSignedIn, setIsSignedIn] = useState(true);
   const navLinkGroups: NavLink[][] = [
     [
       {
-        onClick: () => {},
+        onPress: () => {},
         icon: <IconUser size={21} color="gray" />,
         title: "Profile",
         textColor: "default",
       },
       {
-        onClick: () => {},
+        onPress: () => {},
         icon: <IconSend size={21} color="gray" />,
         title: "Submissions",
         textColor: "default",
@@ -37,13 +44,32 @@ export const ProfileButton = () => {
     ],
     [
       {
-        onClick: () => {},
+        onPress: () => {
+          setIsSignedIn(false);
+        },
         icon: <IconLogout size={21} color="#f31260" />,
         title: "Sign Out",
         textColor: "danger",
       },
     ],
   ];
+
+  if (!isSignedIn) {
+    return (
+      <Popover placement="bottom-end" showArrow={true}>
+        <PopoverTrigger>
+          <Button className="rounded-full bg-transparent" isIconOnly>
+            <Avatar showFallback src="test" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[250px] max-w-full py-[20px]">
+          <Button color="primary" onPress={() => setIsSignedIn(true)}>
+            <IconBrandGoogleFilled /> Sign in with Google
+          </Button>
+        </PopoverContent>
+      </Popover>
+    );
+  }
 
   return (
     <Popover placement="bottom-end" showArrow={true}>
@@ -54,7 +80,7 @@ export const ProfileButton = () => {
       </PopoverTrigger>
       <PopoverContent className="w-[350px] max-w-full">
         <div className="w-full py-2">
-          <div className="my-[20px] flex flex-col items-center gap-[10px]">
+          <div className="mt-[20px] flex flex-col items-center gap-[10px]">
             <Avatar
               isBordered
               showFallback
@@ -64,12 +90,22 @@ export const ProfileButton = () => {
             />
             <b className="text-xl">Hello, Player</b>
           </div>
+          <div className="my-[15px] flex justify-center">
+            <Button className="font-bold" color="primary" variant="flat">
+              <IconPlus /> Submit new record
+            </Button>
+          </div>
+
           {navLinkGroups.map((group, index) => {
             return (
               <div key={index}>
                 {group.map((item) => {
                   return (
-                    <Button key={item.title} className="h-[35px] w-full justify-start bg-transparent px-3 hover:bg-default">
+                    <Button
+                      key={item.title}
+                      className="h-[35px] w-full justify-start bg-transparent px-3 hover:bg-default"
+                      onPress={item.onPress}
+                    >
                       {item.icon}{" "}
                       <span
                         className={
